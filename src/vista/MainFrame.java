0de9a4cf9234e2615a926270bfd1ea916acd162f
Julito01 <julitod55@hotmail.com>
2022-05-18 15:11:20 -0300

    Created a tabbed pane to navigate through the different options of the application

--- src/vista/MainFrame.java
+++ src/vista/MainFrame.java
@@ -1,7 +1,77 @@
 package vista;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
-public class MainFrame {
+public class MainFrame extends JFrame {
     private JPanel mainPanel;
+    private JLabel userLabel;
+    private JLabel passLabel;
+    private JPanel header;
+    private JPanel exitButton;
+    private JLabel exitLabel;
+    private JTabbedPane optionsPane;
+    private JPanel patientTab;
+    private JPanel stationTab;
+    private JPanel practiceTab;
+    private JPanel petitionTab;
+    private JPanel resultTab;
+    private JPanel userTab;
+    private int xMouse, yMouse;
+
+
+    public MainFrame(String username, String password) {
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.setSize(1280, 800);
+        this.getContentPane().setBackground(LoginFrame.bgColor);
+        this.setResizable(false);
+        this.setUndecorated(true);
+        this.bindEvents();
+        exitLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        this.setLocationRelativeTo(null);
+    }
+
+    private void bindEvents() {
+        header.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                xMouse = e.getX();
+                yMouse = e.getY();
+            }
+        });
+        header.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                int x = e.getXOnScreen();
+                int y = e.getYOnScreen();
+                setLocation(x - xMouse, y - yMouse);
+            }
+        });
+
+        // Event to add styles to the exit button when hover it
+        exitLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
+            }
+        });
+        exitLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                exitButton.setBackground(Color.red);
+                exitLabel.setForeground(Color.black);
+            }
+        });
+        exitLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseExited(MouseEvent e) {
+                exitButton.setBackground(LoginFrame.bgColor);
+                exitLabel.setForeground(Color.white);
+            }
+        });
+    }
 }
