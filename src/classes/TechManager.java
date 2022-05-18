2e299f913a18dcb7911468c7e1896394f570667d
Julito01 <julitod55@hotmail.com>
2022-05-18 15:05:11 -0300

    Changed constructor

--- src/classes/TechManager.java
+++ src/classes/TechManager.java
@@ -1,9 +1,10 @@
 package classes;
 
 public class TechManager extends SystemUser {
-    private int techManagerId;
+    private final int techManagerId;
 
     public TechManager(int techManagerId) {
+        super();
         this.techManagerId = techManagerId;
     }
 }
