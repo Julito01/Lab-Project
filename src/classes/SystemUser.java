db17c50f69d810f9a26989f7e42f76d4003b53cc
Julito01 <julitod55@hotmail.com>
2022-05-20 23:53:35 -0300

    Added methods to validate if user exists and user type

--- src/classes/SystemUser.java
+++ src/classes/SystemUser.java
@@ -1,6 +1,13 @@
 package classes;
 import classes.enumerations.UserTypeEnum;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvValidationException;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +17,10 @@ public class SystemUser extends Person {
     private String username;
     private String password;
     private LocalDate birthdate;
-    private UserTypeEnum userType;
-    private List<SystemUser> systemUsers = new ArrayList<SystemUser>();
+    private static UserTypeEnum userType;
+    private static SystemUser self;
+    private static String csvPath = "/Users/julito/Documents/Tecnicatura de Software/Paradigma Orientado a Objetos/Projecto-Laboratorio/users.csv";
+    private static List<String[]> systemUsers = new ArrayList<String[]>();
 
     public SystemUser() {}
 
@@ -22,12 +31,70 @@ public class SystemUser extends Person {
         this.password = password;
         this.birthdate = birthdate;
         this.userType = userType;
+        this.self = this;
         createSystemUser(this);
     }
 
     private void createSystemUser(SystemUser user) {
-        // Creates a new user in the system
-        systemUsers.add(user);
+        // Creates a new user in the system and adds it to users.csv
+        try {
+            CSVWriter writer = new CSVWriter(new FileWriter(csvPath, true));
+            String[] userData = {user.username, user.password, user.userType.toString()};
+            writer.writeNext(userData);
+            writer.close();
+        }
+        catch (FileNotFoundException error) {
+            System.out.println("No se encontro el archivo: " + error);
+        }
+        catch (IOException error) {
+            System.out.println("Error: " + error);
+        }
+    }
+
+    public static void getUsers() {
+        try {
+            CSVReader reader = new CSVReader(new FileReader(csvPath));
+            String[] fila;
+            while ((fila = reader.readNext()) != null) {
+                if (fila != null){
+                    String[] userData = {fila[0], fila[1], fila[2]};
+                    systemUsers.add(userData);
+                }
+            }
+            reader.close();
+        }
+        catch (FileNotFoundException error) {
+            System.out.println("No se encontro el archivo: " + error);
+        }
+        catch (IOException error) {
+            System.out.println("Error: " + error);
+        }
+        catch (CsvValidationException error) {
+            System.out.println("Error: " + error);
+        }
+        catch (NoClassDefFoundError error) {
+            System.out.println("Error: " + error);
+        }
+    }
+
+    public static String verifyUserType(String username) {
+        String typeOfUser = null;
+        for (int i = 0; i < systemUsers.size(); i++) {
+            if (username.equals(systemUsers.get(i)[0])) {
+                typeOfUser = systemUsers.get(i)[2];
+            }
+        }
+        return typeOfUser;
+    }
+
+    public static boolean verifyUserExist(String username) {
+        getUsers();
+        for (int i = 0; i < systemUsers.size(); i++) {
+            if (username.equals(systemUsers.get(i)[0])) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void deleteSystemUser() {
