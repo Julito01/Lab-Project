2f12ac920c4abab0c07bdf33d9f539c23c7b0f7f
Julio Dechert <julitod55@hotmail.com>
2022-06-06 02:35:05 -0300

    Retrieving the practice id and also CRUD of practice available if user is admin

--- src/classes/Practice.java
+++ src/classes/Practice.java
@@ -1,24 +1,23 @@
 package classes;
 import java.util.*;
 import classes.enumerations.ResultType;
+import config.Database;
 
 import javax.xml.transform.Result;
 
 public class Practice {
-    private int practiceId;
-    private int practiceCode;
-    private String practiceName;
-    private ArrayList group;
-    private Date resultETD;
+    private static int practiceId;
+    private int counter = 1;
+    private static int practCodeCounter = 1000;
+    private static int practiceCode;
+    private static String practiceName;
 
-    public Practice() {}
-
-    public Practice(int practiceId, int practiceCode, String practiceName, ArrayList group, Date resultETD) {
-        this.practiceId = practiceId;
-        this.practiceCode = practiceCode;
-        this.practiceName = practiceName;
-        this.group = group;
-        this.resultETD = resultETD;
+    public Practice(String practiceName) {
+        practiceId = counter;
+        practiceCode = practCodeCounter;
+        Practice.practiceName = practiceName;
+        counter++;
+        practCodeCounter++;
     }
 
     protected void getResultType() {
@@ -27,14 +26,35 @@ public class Practice {
     }
 
     private void createPractice() {
-        // Creates a new practice
+        Database.createPractice();
     }
 
     private void deletePractice() {
-        // Deletes a practice through the practice id
+        Database.deletePractice();
     }
 
     private void editPractice() {
-        // Modifies a practice through the practice id
+        Database.editPractice();
+    }
+
+    public static void setPracticePetition(Patient patient, Petition petition, int practId) {
+        Database.createPacientPetition(patient, petition, practId);
+    }
+
+    public static int getCurrPracticeId(String practiceName) {
+        int practiceData = Database.getPracticeId(practiceName);
+        return practiceData;
+    }
+
+    public static int getPracticeId() {
+        return practiceId;
+    }
+
+    public static int getPracticeCode() {
+        return practiceCode;
+    }
+
+    public static String getPracticeName() {
+        return practiceName;
     }
 }
