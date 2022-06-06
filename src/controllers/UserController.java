9c41ceabd9de891e12588a13f313ee63035300f3
Julio Dechert <julitod55@hotmail.com>
2022-06-06 02:37:03 -0300

    Added controllers

--- src/controllers/UserController.java
+++ src/controllers/UserController.java
@@ -11,20 +11,24 @@ public class UserController {
     private String password;
     private UserTypeEnum userType;
 
-    public static void createUser(String username, String password1, String password2, LocalDate birthdate, UserTypeEnum userType) {
+    public static boolean createUser(String username, String password1, String password2, LocalDate birthdate, UserTypeEnum userType) {
         // User validations
         if (verifyUsername(username) && verifyPass(password1, password2) && verifyUserType(userType) && !SystemUser.verifyUserExist(username)) {
             SystemUser newUser = new SystemUser(username, password1, userType);
             JOptionPane.showMessageDialog(null, "Usuario creado con exito.", "Usuario creado", JOptionPane.INFORMATION_MESSAGE);
+            return true;
         }
         else if (username.equals("") || password1.equals("") || password2.equals("") || birthdate == null) {
             JOptionPane.showMessageDialog(null, "Faltan completar campos y/o las contraseñas no coinciden.", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
         }
         else if (SystemUser.verifyUserExist(username)) {
             JOptionPane.showMessageDialog(null, "El usuario ingresado ya existe.", "Usuario existente", JOptionPane.WARNING_MESSAGE);
+            return false;
         }
         else {
             JOptionPane.showMessageDialog(null, "Ingrese un usuario de mas de 4 caracteres.", "Error", JOptionPane.WARNING_MESSAGE);
+            return false;
         }
     }
     private static boolean verifyUsername(String username) {
