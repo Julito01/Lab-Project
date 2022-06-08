124d2156ddc9f70d2325f7b6a7424a75c86b552a
Julio Dechert <julitod55@hotmail.com>
2022-06-08 18:34:18 -0300

    Patients are now stored in the database

--- src/classes/Patient.java
+++ src/classes/Patient.java
@@ -1,2 +1,58 @@
-package classes;public class Patient {
+package classes;
+import config.Database;
+
+public class Patient extends Person {
+    private String patientId;
+    private String name;
+    private String address;
+    private String mail;
+    private String genre;
+    private String age;
+    private Patient self;
+
+    public Patient(String patientId, String name, String address, String mail, String genre, String age) {
+        this.patientId = patientId;
+        this.name = name;
+        this.address = address;
+        this.mail = mail;
+        this.genre = genre;
+        this.age = age;
+        this.self = this;
+        createPatient(this);
+    }
+    private void createPatient(Patient patient) {
+        Database.createPatient(patient);
+    }
+
+    private void deletePatient() {
+        // Deletes a desired patient through the patient id
+    }
+
+    private void editPatient() {
+        // Modifies the data of a desired patient through the patient id
+    }
+
+    public String getPatientId() {
+        return this.patientId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getMail() {
+        return this.mail;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public String getAge() {
+        return this.age;
+    }
 }
