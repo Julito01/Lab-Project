545e5c74bd80cfc8527d50434419738b67a0e010
Julio Dechert <julitod55@hotmail.com>
2022-06-08 18:36:13 -0300

    Implemented DTO

--- src/dtos/PatientDTO.java
+++ src/dtos/PatientDTO.java
@@ -1,2 +1,87 @@
-package dtos;public class PatientDTO {
+package dtos;
+
+import classes.Patient;
+import config.Database;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PatientDTO {
+    private String patientId;
+    private String name;
+    private String address;
+    private String mail;
+    private String genre;
+    private String age;
+    private PatientDTO self;
+    private List<PetitionDTO> petitionsArray = new ArrayList<>();
+
+    public PatientDTO(String patientId, String name, String address, String mail, String genre, String age) {
+        this.patientId = patientId;
+        this.name = name;
+        this.address = address;
+        this.mail = mail;
+        this.genre = genre;
+        this.age = age;
+        this.self = self;
+    }
+
+    public void addPetitions(PetitionDTO petition) {
+        petitionsArray.add(petition);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public static List<PatientDTO> getPatients() {
+        List<PatientDTO> patients = Database.getAllPatients();
+        return patients;
+    }
+
+    public static void createPatient(PatientDTO patientDTO) {
+        String patientId = patientDTO.getPatientId();
+        String name = patientDTO.getName();
+        String address = patientDTO.getAddress();
+        String mail = patientDTO.getMail();
+        String genre = patientDTO.genre;
+        String age = patientDTO.getAge();
+
+        Patient patient = new Patient(patientId, name, address, mail, genre, age);
+        Database.createPatient(patient);
+    }
+
+    public static void deletePatient(String patientId) {
+        // Deletes a desired patient through the patient id
+        Database.deletePatient(patientId);
+    }
+
+    private void editPatient() {
+        // Modifies the data of a desired patient through the patient id
+    }
+
+    // Getters
+    public String getPatientId() {
+        return this.patientId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getMail() {
+        return this.mail;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public String getAge() {
+        return this.age;
+    }
 }
--- src/dtos/PetitionDTO.java
+++ src/dtos/PetitionDTO.java
@@ -1,2 +1,80 @@
-package dtos;public class PetitionDTO {
+package dtos;
+
+import classes.Petition;
+import classes.Practice;
+import config.Database;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class PetitionDTO {
+    private int petitionId;
+    private static String medInsurance;
+    private static LocalDate loadDate;
+    private static LocalDate deliverDate;
+    private static List<String> practices;
+    private boolean petitionCompleted;
+    private static int counter = 1;
+    private PetitionDTO self;
+
+    public PetitionDTO(String medInsurance, LocalDate loadDate, List<String> practices) {
+        this.petitionId = this.counter;
+        this.medInsurance = medInsurance;
+        this.loadDate = loadDate;
+        this.practices = practices;
+        this.deliverDate = getDeliveryDate(this.loadDate, this.practices);
+        this.petitionCompleted = false;
+        createPetition(this);
+        this.counter++;
+    }
+
+    private void createPetition(PetitionDTO petitionDTO) {
+        String medInsurance = petitionDTO.getMedInsurance();
+        LocalDate loadDate = petitionDTO.getLoadDate();
+        List<String> practices = petitionDTO.getPractices();
+
+        Petition petition = new Petition(medInsurance, loadDate, practices);
+    }
+
+    public static void createPatientPetition(String patientId, int petitionId, String practiceName) {
+        for (int i = 0; i < practices.size(); i++) {
+            int practId = Practice.getCurrPracticeId(practiceName);
+            Database.createPatientPetition(patientId, petitionId, practId);
+        }
+    }
+
+    private void deletePetition() {
+        // Deletes a desired petition through the petition id
+    }
+
+    private void editPetition() {
+        // Modifies the data of a petition through the petition id
+    }
+
+    public static int getPetitionId() {
+        return counter;
+    }
+
+    private static List<String> getPractices() {
+        return practices;
+    }
+
+    public static String getMedInsurance() {
+        return medInsurance;
+    }
+
+    public static LocalDate getLoadDate() {
+        return loadDate;
+    }
+
+    public static LocalDate getEtaDate() {
+        return deliverDate;
+    }
+
+    private static LocalDate getDeliveryDate(LocalDate date, List<String> practicesArray) {
+        for (int i = 0; i < practicesArray.size(); i++) {
+            date.plusDays(2);
+        }
+        return date;
+    }
 }
