6daec09a3600a9df8de89187c2304f68cf8c2b2d
Julio Dechert <julitod55@hotmail.com>
2022-06-08 18:37:16 -0300

    New frame to create a patient being admin user type

--- src/vista/AddPatientFrame.java
+++ src/vista/AddPatientFrame.java
@@ -1,12 +1,15 @@
 package vista;
 
-import classes.Patient;
 import controllers.PatientController;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-public class AddPatient {
+public class AddPatientFrame extends JDialog {
     private JPanel mainPanel;
     private JTextField dniField;
     private JTextField nameField;
@@ -21,9 +24,12 @@ public class AddPatient {
     private JLabel genreLabel;
     private JLabel ageLabel;
     private JButton addPatientButton;
+    private PatientController cp;
+    private AddPatientFrame self;
 
-    public AddPatient(Window owner, String title) {
+    public AddPatientFrame(Window owner, String title) {
         super(owner, title);
+        this.cp = PatientController.getInstance();
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.setSize(460, 300);
@@ -31,5 +37,38 @@ public class AddPatient {
         this.setModal(true);
         this.self = this;
         this.bindEvents();
+        setGenreModel();
+    }
+
+    private void setGenreModel() {
+        List<String> genres = new ArrayList<>();
+        genres.add("M");
+        genres.add("F");
+        genres.add("Otro");
+        DefaultComboBoxModel model = new DefaultComboBoxModel();
+        model.addAll(genres);
+        genreField.setModel(model);
+    }
+
+    private void bindEvents() {
+        addPatientButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String id = dniField.getText();
+                String name = nameField.getText();
+                String address = addressField.getText();
+                String email = emailField.getText();
+                String genre = genreField.getSelectedItem().toString();
+                String age = ageField.getText();
+
+                if (cp.setPatient(id, name, address, email, genre, age)) {
+                    JOptionPane.showMessageDialog(null, "Paciente agregado con éxito.", "Paciente agregado", JOptionPane.INFORMATION_MESSAGE);
+                    self.dispose();
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "Faltan completar campos.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
     }
 }
