a00bfa7f9bea5a9f06d84d51a656b98d200bb9bf
Julio Dechert <julitod55@hotmail.com>
2022-06-14 20:56:21 -0300

    Changed the constructor, the patient has an id now

--- src/dtos/PatientDTO.java
+++ src/dtos/PatientDTO.java
@@ -1,36 +1,47 @@
 package dtos;
 
 import classes.Patient;
-import config.Database;
 import controllers.PatientController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PatientDTO {
-    private String patientId;
+    private int patientId;
+    private String patientDni;
     private String name;
     private String address;
     private String mail;
     private String genre;
     private String age;
-    private PatientDTO self;
     private PatientController patInstance;
     private List<PetitionDTO> petitionsArray = new ArrayList<>();
+    private PatientDTO self;
+    private static int counter = 1;
 
-    public PatientDTO(String patientId, String name, String address, String mail, String genre, String age) {
+    public PatientDTO(String patientDni, String name, String address, String mail, String genre, String age) {
+        this.patientId = counter;
         this.patInstance = PatientController.getInstance();
-        this.patientId = patientId;
+        this.patientDni = patientDni;
         this.name = name;
         this.address = address;
         this.mail = mail;
         this.genre = genre;
         this.age = age;
-        createPatient(this);
+        this.self = this;
+        counter++;
+    }
+
+    public PatientDTO getPatientDTO() {
+        return this.self;
     }
 
     public void addPetitions(PetitionDTO petition) {
         petitionsArray.add(petition);
+    }
+
+    public int getPatientId() {
+        return this.patientId;
     }
 
     @Override
@@ -42,18 +53,13 @@ public class PatientDTO {
         Patient patient = new Patient(patientDTO);
     }
 
-    public void deletePatient(String patientId) {
-        // Deletes a desired patient through the patient id
-        patInstance.deletePatient(patientId);
-    }
-
     private void editPatient() {
         // Modifies the data of a desired patient through the patient id
     }
 
     // Getters
-    public String getPatientId() {
-        return this.patientId;
+    public String getPatientDni() {
+        return this.patientDni;
     }
 
     public String getName() {
@@ -74,5 +80,31 @@ public class PatientDTO {
 
     public String getAge() {
         return this.age;
+    }
+
+    // Setters
+
+    public void setPatientDni(String patientDni) {
+        this.patientDni = patientDni;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
     }
 }
