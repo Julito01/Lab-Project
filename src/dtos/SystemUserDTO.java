4e9b0a5ad5ea0addb7b899214ee5d965de30d29d
Julio Dechert <julitod55@hotmail.com>
2022-06-15 14:23:03 -0300

    Override toString method

--- src/dtos/SystemUserDTO.java
+++ src/dtos/SystemUserDTO.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SystemUserDTO extends Person {
+    private static int counter;
     private int systemUserId;
     private String username;
     private String password;
@@ -16,11 +17,12 @@ public class SystemUserDTO extends Person {
     private static List<SystemUserDTO> systemUsers = new ArrayList<>();
 
     public SystemUserDTO(String username, String password, UserTypeEnum userType) {
+        this.systemUserId = counter;
         this.username = username;
         this.password = password;
         this.userType = userType;
+        counter++;
     }
-
     public UserTypeEnum verifyUserType(String username) {
         UserTypeEnum typeOfUser = null;
         for (int i = 0; i < systemUsers.size(); i++) {
@@ -41,6 +43,10 @@ public class SystemUserDTO extends Person {
         return false;
     }
 
+    @Override
+    public String toString() {
+        return this.username;
+    }
     private void deleteSystemUser() {
         // Deletes a desired user through user id
     }
@@ -49,6 +55,10 @@ public class SystemUserDTO extends Person {
         // Modifies the permissions or data of a user through the user id
     }
 
+    //Getters
+    public int getUserId() {
+        return this.systemUserId;
+    }
     public String getUsername() {
         return this.username;
     }
