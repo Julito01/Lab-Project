5b9f46823dc0b310507f89fe0191ab35aca66d91
Julio Dechert <julitod55@hotmail.com>
2022-06-15 10:18:22 -0300

    Created petition frames

--- src/vista/EditPetitionFrame.java
+++ src/vista/EditPetitionFrame.java
@@ -1,2 +1,52 @@
-package vista;public class EditPetitionFrame {
+package vista;
+
+import com.github.lgooddatepicker.components.DatePicker;
+import controllers.PetitionController;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class EditPetitionFrame extends JDialog {
+    private EditPetitionFrame self;
+    private PetitionController petInstance;
+    private JPanel mainPanel;
+    private JLabel dniLabel;
+    private JTextField dniField;
+    private JLabel nameLabel;
+    private JTextField nameField;
+    private JLabel addressLabel;
+    private JTextField addressField;
+    private JLabel emailLabel;
+    private JTextField emailField;
+    private JLabel genreLabel;
+    private JComboBox genreBoxField;
+    private JLabel ageLabel;
+    private JTextField ageField;
+    private JLabel medInsLabel;
+    private JLabel loadDateLabel;
+    private DatePicker loadDateField;
+    private JLabel practLabel;
+    private JComboBox practField;
+    private JLabel etaLabel;
+    private JTextField etaField;
+    private JButton createPetitionButton;
+    private JComboBox medInsField;
+    private JLabel addLabel;
+    private JLabel practAddedLabel;
+
+    public EditPetitionFrame(Window owner, String title) {
+        super(owner, title);
+        this.petInstance = PetitionController.getInstance();
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setContentPane(mainPanel);
+//        this.setSize(600, 300);
+        this.setResizable(false);
+        this.setModal(true);
+        this.self = this;
+        this.bindEvents();
+        this.pack();
+        this.setLocationRelativeTo(null);
+    }
+
+    private void bindEvents() {}
 }
