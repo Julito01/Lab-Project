b8bfd9cd35d8a74bd7a07f7caa572ed1105f9328
Julio Dechert <julitod55@hotmail.com>
2022-06-15 10:16:51 -0300

    Created station frame

--- src/vista/AddStationFrame.java
+++ src/vista/AddStationFrame.java
@@ -1,2 +1,50 @@
-package vista;public class AddStationFrame {
+package vista;
+
+import controllers.StationController;
+import dtos.StationDTO;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class AddStationFrame extends JDialog {
+    private JPanel mainPanel;
+    private JLabel addressLabel;
+    private JTextField addressField;
+    private JTextField techUserField;
+    private JLabel techUserLabel;
+    private JButton createStationButton;
+    private StationController sucInstance;
+    private AddStationFrame self;
+
+    public AddStationFrame(Window owner, String title) {
+        super(owner, title);
+        this.sucInstance = StationController.getInstance();
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.setSize(460, 300);
+        this.setResizable(false);
+        this.setModal(true);
+        this.self = this;
+        this.bindEvents();
+        this.setLocationRelativeTo(null);
+    }
+
+    private void bindEvents() {
+        createStationButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                String address = addressField.getText();
+                int techUserId = Integer.parseInt(techUserField.getText());
+                if (sucInstance.setStation(new StationDTO(address, techUserId))) {
+                    JOptionPane.showMessageDialog(null, "Sucursal agregada con éxito", "Sucursal agregada", JOptionPane.INFORMATION_MESSAGE);
+                    self.dispose();
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "Faltan completar campos.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+    }
 }
